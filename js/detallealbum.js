1d4281560cdf529dcd7a77272769cd86e647f27d
Antonella Plati <Christian@MacBook-Air-de-Antonella.local>
2023-05-22 17:28:20 -0300

    terminando el header

--- js/detallealbum.js
+++ js/detallealbum.js
@@ -1,9 +1,10 @@
 function formulario() {
-    let termino = document.querySelectorAll('form-result').value;
-    if (termino.length < 3) {
+    let termino = document.querySelectorAll('.form-result');
+    if (termino.value.length < 3) {
         alert ("Debe ingresar al menos 3 caracteres")
         return false;
     }
 
     return true;
 }
+
