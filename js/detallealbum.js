e53f8f82879db9e9bec53bf239779234b3fd99f6
Antonella Plati <Christian@MacBook-Air-de-Antonella.local>
2023-05-22 16:42:18 -0300

    haciendo header y footer

--- js/detallealbum.js
+++ js/detallealbum.js
@@ -1,1 +1,9 @@
-console.log();
+function formulario() {
+    let termino = document.querySelectorAll('form-result').value;
+    if (termino.length < 3) {
+        alert ("Debe ingresar al menos 3 caracteres")
+        return false;
+    }
+
+    return true;
+}
