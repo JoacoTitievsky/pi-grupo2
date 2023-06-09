cc98dd73dc6f0afdb1b4318279b7dc714d6e18fd
Pilar Canil <sofiaibarrafigueroa@MacBook-Air-de-SOFIA-2.local>
2023-06-09 17:28:28 -0300

    js index

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ let endpoint_album = "https://api.allorigins.win/raw?url=https://developers.deez
 let endpoint_artista = "https://api.allorigins.win/raw?url=https://developers.deezer.com/api/artist"
 let endpoint_tracks = "https://api.allorigins.win/raw?url=https://developers.deezer.com/api/track"
 
+let url = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart"
 
 let buscador = document.querySelector('#buscador');
 let campoBuscar = document.querySelector('#textoBuscado');
@@ -19,24 +20,48 @@ if(campoBuscar.value.length == 0) {
 });
 
 let topalbums = ""
+let imgcancion= document.querySelector('.imgcancion')
 
-fetch(endpoint_album)
+fetch(url)
     .then(function (response) {
         return response.json()
     })
     .then(function (data) {
         console.log(data.results);
 
+        rellenarCanciones(data.tracks)
+        rellenarCanciones(data.albums)
+        rellenarCanciones(data.artists)
+        rellenarCanciones(data.playlists)
+
+
+
         let album = ""
         for (let index = 0; index < data.results.length; index++) {
-            album += `<a href = "./detallealbum.html?id=${data.result[index].id}">
-            
+            album += `<a href = "./detallealbum.html?id=${data.results[index].id}">
+            <img class= "imgcancion" src=${data.results[index]} alt=""/>
+            <h3 class="nombreartista">${data.results[index].title}</h3>
+            <h4 class="nombrealbum">${data.results[index].artist.name}</h4>
             </a>`
         }
-        personajes.innerHTML= album
+        
     })
     .catch(function (error) {
         console.log("Error: " + error);
     })
+
+
+function rellenarCanciones(data) {
+    
+}
+function rellenarCanciones(data) {
+    
+}
+function rellenarCanciones(data) {
+    
+}
+function rellenarCanciones(data) {
+    
+}
 
 
