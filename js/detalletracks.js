7b1c17c93c33baabf6ac28e8b0dd680c1dc53c71
Pilar Canil <sofiaibarrafigueroa@MacBook-Air-de-SOFIA-2.local>
2023-06-12 15:37:09 -0300

    detalle  tracks js

--- js/detalletracks.js
+++ js/detalletracks.js
@@ -2,7 +2,7 @@ let qs = location.search;
 let qsObj = new URLSearchParams(qs)
 let id = qsObj.get('id');
 
-let url = `https://api.allorigins.win/raw?url=https://api.deezer.com/track/3135556${id}` ;
+let url = `https://api.allorigins.win/raw?url=https://api.deezer.com/track/${id}`;
 
 
 fetch(url)
@@ -10,11 +10,26 @@ fetch(url)
         return response.json()
     })
     .then(function (data) {
-    console.log(data);
-        // let detailcancion = document.querySelector(".detailcancion")
-        // detailcancion.innerText = data.tracks.data;
+        console.log(data);
+        let canciones = data.tracks
+        let a = document.querySelector(".tracklist")
 
+        let queryString = location.search;
+        let queryStringObj = new URLSearchParams(queryString);
+        let i = queryStringObj.get('id') - 1;
+
+        let character = `<article>
+        <img src= ${objeto[index].image} alt='' />
+        <p>ID = ${objeto[index].id}</p><p>Name: ${personajes[i].name}</p>
+        </article>`
+        a.innerHTML = character;
+
+        let nombre = personajes[i].name
+        document.querySelector("h1").innerText = `Detalle de ${personajes[i].name}`
+        h1.innerHTML += nombre
     })
     .catch(function (error) {
         console.log(error);
     });
+
+    // FALTA TERMINAR
--- js/resultadobusqueda.js
+++ js/resultadobusqueda.js
@@ -1,9 +1,9 @@
 let queryString = location.search
-let queryStringObj = new URLSearchParams (queryString)
+let queryStringObj = new URLSearchParams(queryString)
 let valor = queryStringObj.get("buscar")
 console.log(valor)
 
-let url = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart" + valor;
+let url = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart" + id
 let resultscanciones = document.querySelector(".resultscanciones")
 
 fetch(url)
@@ -14,27 +14,61 @@ fetch(url)
     .then(function (data) {
         console.log(data);
         document.querySelector(".searchcancion").innerHTML += `<h3 class="titulosearch">
-        Termino buscado : "${valor}"
-         </h3>
-        <h3 class="titulosearch">
-         Resultados que coinciden:
-        </h3>`
+    Termino buscado : "${valor}"
+</h3>
+<h3 class="titulosearch">
+    Resultados que coinciden:
+</h3>`
 
         if (data.data.length == 0) {
-            let searchartista = document.querySelector(".searchartista")
-            let searchalbum = document.querySelector(".searchalbum")
-            searchartista.style.display = "none"
-            searchalbum.style.display = "none"
+            let searchcancion = document.querySelector(".searchcancion")
+            searchcancion.style.display = "none"
         } else {
             for (let index = 0; index < data.data.length; index++) {
-                document.querySelector(".resultsartistas").innerHTML += `<a href = "./detallecancion.html?id=${data.data[index].id}">
+                document.querySelector(`.resultscanciones`).innerHTML += `<a href = "./detallecancion.html?id=${data.data[index].id}">
         <img class= "imgcancion" src=${data.data[index].title} alt=""/>
         <h3 class="nombreartista">${data.data[index].artists}</h3>
         <h4 class="nombrealbum">${data.data[index].albums}</h4>
         </a>`
             }
+            let noSearch = document.querySelector(".noSearch")
+            noSearch.style.display = "none"
         }
     })
     .catch(function (error) {
         console.log(error);
     });
+
+let urlArtista =
+    fetch(urlArtista)
+        .then(function (response) {
+            return response.json()
+        })
+
+        .then(function (data) {
+            console.log(data);
+            document.querySelector(".searchArtista").innerHTML += `<h3 class="titulosearch">
+        Termino buscado : "${valor}"
+        </h3>
+        <h3 class="titulosearch">
+        Resultados que coinciden:
+        </h3>`
+
+            if (data.data.length == 0) {
+                let searchartista = document.querySelector(".searchartista")
+                searchartista.style.display = "none"
+            } else {
+                for (let index = 0; index < data.data.length; index++) {
+                    document.querySelector(`.resultsartistas`).innerHTML += `<a href = "./detallecantante.html?id=${data.data[index].id}">
+                <img class= "imgcancion" src=${data.data[index].title} alt=""/>
+                <h3 class="nombreartista">${data.data[index].artists}</h3>
+                <h4 class="nombrealbum">${data.data[index].albums}</h4>
+                </a>`
+                }
+                let noSearch = document.querySelector(".noSearch")
+                noSearch.style.display = "none"
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
