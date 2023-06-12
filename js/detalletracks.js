e7cced09b3a6c5c830cbf5dc5dabda036592c827
Antonella Plati <Christian@MacBook-Air-de-Antonella.local>
2023-06-12 14:23:18 -0300

    empezando detalletracks js

--- js/detalletracks.js
+++ js/detalletracks.js
@@ -10,17 +10,8 @@ fetch(url)
     })
     .then(function (data) {
         console.log(data);
-        // let cancion = document.querySelector(".cancion")
-        // cancion.innerText = data.title;
-
-        // let album = document.querySelector(".album")
-        // album.innerText = data.album.title;
-
-        // let artista = document.querySelector(".artista")
-        // artista.innerText = data.artist.name;
-
-        // let imagen = document.querySelector(".imagen")
-        // imagen.src = data.album.cover;
+        let cancion = document.querySelector(".cancion")
+        cancion.innerText = data.tracks.data;
 
     })
     .catch(function (error) {
